d82379e92617f79710865a1609908dfc05277696
Joshua de Guzman <jdeguzman@freelancer.com>
2019-09-23 14:50:03 +0800

    Add more unit tests

--- test/fl_project_test.dart
+++ test/fl_project_test.dart
@@ -11,13 +11,19 @@ void main() async {
   final testResourceFile = File('test_resources/fl_project_data.json');
   final json = jsonDecode(await testResourceFile.readAsString());
   final project = FLProject.fromJson(json);
-  
+
   group('projects', () {
     test('evaluate general project data', () async {
       expect(project.hidebids, false);
       expect(project.bidperiod, 7);
       expect(project.bidperiod, 7);
       expect(project.featured, true);
+      expect(project.frontendProjectStatus, "open");
+      expect(project.deleted, false);
+      expect(project.local, false);
+      expect(project.location, null);
+      expect(project.isSellerKycRequired, false);
+      expect(project.timeSubmitted, 1569271320);
     });
 
     test('evaluate project currency data', () async {
@@ -32,11 +38,28 @@ void main() async {
     });
 
     test('evaluate project upgrades data', () async {
+      expect(project.upgrades.nDA, false);
       expect(project.upgrades.urgent, false);
       expect(project.upgrades.featured, true);
       expect(project.upgrades.nonpublic, false);
       expect(project.upgrades.fulltime, false);
+      expect(project.upgrades.qualified, false);
       expect(project.upgrades.sealed, false);
+      expect(project.upgrades.pfOnly, false);
+      expect(project.upgrades.ipContract, false);
+    });
+
+    test('evaluate project budget data', () async {
+      expect(project.budget.currencyId, null);
+      expect(project.budget.minimum, 50.0);
+      expect(project.budget.maximum, 1000.0);
+      expect(project.budget.projectType, null);
+      expect(project.budget.name, null);
+    });
+
+    test('evaluate project bid stats data', () async {
+      expect(project.bidStats.bidCount, 45);
+      expect(project.bidStats.bidAvg, 772.6666666666666);
     });
 
     // TODO: Add more tests
